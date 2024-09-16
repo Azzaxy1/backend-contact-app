dd2ab372c458fc493e0fa57ebed8a32a33f9c908
Azzaxy <abdurrohmanazis@gmail.com>
2024-09-16 09:50:38 +0700

    study case: adding warning when running node app

--- contact-app/app.js
+++ contact-app/app.js
@@ -1,29 +1,31 @@
 const yargs = require("yargs");
 const { simpanContact } = require("./contacts");
 
-yargs.command({
-  command: "add",
-  describe: "Menambahkan contact baru",
-  builder: {
-    nama: {
-      describe: "Nama Lengkap",
-      demandOption: true,
-      type: "string",
+yargs
+  .command({
+    command: "add",
+    describe: "Menambahkan contact baru",
+    builder: {
+      nama: {
+        describe: "Nama Lengkap",
+        demandOption: true,
+        type: "string",
+      },
+      email: {
+        describe: "Email",
+        demandOption: false,
+        type: "string",
+      },
+      noHP: {
+        describe: "Nomor Handphone",
+        demandOption: true,
+        type: "string",
+      },
     },
-    email: {
-      describe: "Email",
-      demandOption: false,
-      type: "string",
+    handler(argv) {
+      simpanContact(argv.nama, argv.email, argv.noHP);
     },
-    noHP: {
-      describe: "Nomor Handphone",
-      demandOption: true,
-      type: "string",
-    },
-  },
-  handler(argv) {
-    simpanContact(argv.nama, argv.email, argv.noHP);
-  },
-});
+  })
+  .demandCommand();
 
 yargs.parse();
