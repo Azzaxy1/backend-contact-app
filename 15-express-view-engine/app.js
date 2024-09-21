0087022fb86c7a570a0638853d5ac6ff8b70908b
Azzaxy <abdurrohmanazis@gmail.com>
2024-09-21 14:28:08 +0700

    <learn> : view/template engine

--- 15-express-view-engine/app.js
+++ 15-express-view-engine/app.js
@@ -1,14 +1,42 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
 
 const port = 3000;
 
+// gunakan ejs
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+
 app.get("/", (req, res) => {
-  res.sendFile("./index.html", { root: __dirname });
+  // res.sendFile("./index.html", { root: __dirname });
+  const mahasiswa = [
+    {
+      nama: "Azis",
+      email: "azis@gmail.com",
+    },
+    {
+      nama: "Hadi",
+      email: "hadi@gmail.com",
+    },
+    {
+      nama: "Ahmad",
+      email: "ahmad@gmail.com",
+    },
+  ];
+  res.render("index", {
+    nama: "Abdurrohman Azis",
+    title: "Halaman Home",
+    mahasiswa,
+    layout: "layouts/main-layout",
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./about.html", { root: __dirname });
+  res.render("about", {
+    title: "Halaman About",
+    layout: "layouts/main-layout",
+  });
 });
 
 app.get("/product/:id", (req, res) => {
@@ -20,7 +48,10 @@ app.get("/product/:id", (req, res) => {
 });
 
 app.get("/contact", (req, res) => {
-  res.sendFile("./contact.html", { root: __dirname });
+  res.render("contact", {
+    title: "Halaman Contact",
+    layout: "layouts/main-layout",
+  });
 });
 
 app.use("/", (req, res) => {
